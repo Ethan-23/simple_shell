3523d3f11708bcca7c9c604701b8c5eaf56e7b8a
Manuel Figueroa <manuelthemuffin@gmail.com>
2020-11-15 17:11:56 +0000

    added some base files and our simple shell prototype for now

---
+++ simple_shell.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ *main - this is the main file for a simple shell
+ *Return: returns 0 on completion
+ */
+int main(void)
+{
+
+}
